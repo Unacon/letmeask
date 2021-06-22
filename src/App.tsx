1f738ccd3a04ca2693e55aa0d781dd68d04858fb
Unacon <joaovitor_892@hotmail.com>
2021-06-22 20:25:59 -0300

    Adição das imagens e fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Home } from './pages/Home';
 
 import './services/firebase'
+import './assets/css/global.scss'
 
 function App() {
   return (
-    <h1>Hello Word</h1>
+    <Home />
   );
 }
 
